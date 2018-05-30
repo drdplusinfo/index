95e934f513680eb3b32058c635f2c45951f3bce7
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-30 17:08:59 +0200

    Tests config migrated to new format

--- DrdPlus/Tests/tests_config.php
+++ DrdPlus/Tests/tests_config.php
@@ -1,13 +1,14 @@
 <?php
 global $testsConfiguration;
 $testsConfiguration = new \DrdPlus\Tests\RulesSkeleton\TestsConfiguration();
-$testsConfiguration->setHasTables(false);
-$testsConfiguration->setHasAuthors(false);
-$testsConfiguration->setHasProtectedAccess(false);
-$testsConfiguration->setCanBeBoughtOnEshop(false);
-$testsConfiguration->setHasCharacterSheet(false);
-$testsConfiguration->setHasLinksToJournals(false);
-$testsConfiguration->setHasLinkToSingleJournal(false);
-$testsConfiguration->setHasDebugContacts(false);
-$testsConfiguration->setHasIntroduction(false);
+$testsConfiguration->disableHasTables();
+$testsConfiguration->disableHasAuthors();
+$testsConfiguration->disableHasProtectedAccess();
+$testsConfiguration->disableCanBeBoughtOnEshop();
+$testsConfiguration->disableHasCharacterSheet();
+$testsConfiguration->disableHasLinksToJournals();
+$testsConfiguration->disableHasLinkToSingleJournal();
+$testsConfiguration->disableHasDebugContacts();
+$testsConfiguration->disableHasIntroduction();
+$testsConfiguration->disableHasCustomBodyContent();
 $testsConfiguration->setBlockNamesToExpectedContent([]);
