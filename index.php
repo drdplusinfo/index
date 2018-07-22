5d0ab4a5e41a530d2285fe7b8224f322e0ab93bb
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-22 18:01:47 +0200

    Fixed used vesrsion as only single one is used

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ $hasFreeAccess = true;
 $hasContactsFixed = true;
 $hasHiddenHomeButton = true;
 $googleAnalyticsI = 'UA-121206931-1';
+$latestVersion = 'master';
 
 include __DIR__ . '/vendor/drd-plus/rules-skeleton/index.php';
