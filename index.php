ae29fe3f229f328b7df57ae6540babc098a56937
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-22 17:55:46 +0200

    Index file simplified

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
-$documentRoot = $documentRoot ?? (PHP_SAPI !== 'cli' ? \rtrim(\dirname($_SERVER['SCRIPT_FILENAME']), '\/') : \getcwd());
-$webRoot = $webRoot ?? $documentRoot . '/web/passed';
-$vendorRoot = $vendorRoot ?? $documentRoot . '/vendor';
+$hasFreeAccess = true;
+$hasContactsFixed = true;
+$hasHiddenHomeButton = true;
+$googleAnalyticsI = 'UA-121206931-1';
 
-require_once $vendorRoot . '/autoload.php';
-
-$controller = $controller ?? new \DrdPlus\RulesSkeleton\RulesController(
-        'UA-121206931-1',
-        \DrdPlus\RulesSkeleton\HtmlHelper::createFromGlobals($documentRoot),
-        $documentRoot,
-        $webRoot,
-        $vendorRoot
-    );
-$controller->setFreeAccess();
-$controller->setContactsFixed();
-$controller->hideHomeButton();
-
-include $vendorRoot . '/drd-plus/rules-skeleton/index.php';
+include __DIR__ . '/vendor/drd-plus/rules-skeleton/index.php';
