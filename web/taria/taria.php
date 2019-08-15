b2ebd79539fa811442295ccb80637183b9f84ef2
Jaroslav Týc <mail@jaroslavtyc.com>
2019-08-15 18:19:47 +0200

    Text for Taria

---
+++ web/taria/taria.php
@@ -0,0 +1,55 @@
+<?php
+use \DrdPlus\Index\FileTools;
+
+?>
+<div class="container-fluid">
+  <div class="row">
+    <article class="pt-2 pb-4 mb-4 col-md-12">
+      <h1 class="heading-without-id">Taria</h1>
+
+      <p>V pradávných dobách, kdy jsme na začátku letopočtu ještě psali jedničku a Sametová revoluce stále voněla
+        novotou, přičichli tři studenti <a href="https://www.gymzl.cz/">zlínského gymnázia na Lesní
+          čtvrti</a> k Dračímu doupěti. A zážitek to byl pro ně
+        tak silný, že začali tvořit vlastní svět, Tarii.</p>
+
+      <p>Od těch dob tři kamarádi, Pecan, Paja a Charlie, s pomocí mnoha dobrovolníků, stvořili nejen základní
+        modul o
+        <a href="http://taria.unas.cz/infopage.php?page=10">Tarii</a>, ale popsali i její
+        <a href="http://taria.unas.cz/infopage.php?page=2">Záhvozdí</a>,
+        mrazivější <a href="http://taria.unas.cz/infopage.php?page=11">Severní Tarii</a> a její
+        <a href="http://taria.unas.cz/infopage.php?page=11">Pohraničí</a>.
+      </p>
+
+      <p>Bohužel sešlo z připravovaného dalšího modulu <a href="http://taria.unas.cz/infopage.php?page=51">Jihozápad</a>,
+        u kterého můžeme jen doufat, že je to spící drak.</p>
+
+      <p><i>Taria je střízlivý a uvěřitelný fantasy světadíl, který vám netvrdí, že okolo už nic není. Přirozeně vám tak
+          dovoluje zasadit Tarii kamkoli, kde je ještě trochu místa. Je to příjemný protiklad silně epickému a
+          dopodrobna
+          zpracovanému <a href="/asterion">Asterionu</a>.</i></p>
+
+      <h2>Pravidla DrD+ pro moduly Tarie</h2>
+
+      <ul>
+        <li>
+          <a class="link" href="/pdf/taria/taria_zahvozdi_drd_plus.pdf"><span class="name">Taria Záhvozdní - Aplikace pravidel pro DrD+</span>
+            <span class="note">
+              (PDF <?= FileTools::getFileSizeInMegabytes(__DIR__ . '/../../pdf/taria/taria_zahvozdi_drd_plus.pdf') ?>)
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="/pdf/taria/severni_taria_drd_plus.pdf" class="link"><span class="name">Severní Taria - Aplikace pravidel pro DrD+</span>
+            <span class="note">
+              (PDF <?= FileTools::getFileSizeInMegabytes(__DIR__ . '/../../pdf/taria/severni_taria_drd_plus.pdf') ?>)
+            </span>
+          </a>
+        </li>
+      </ul>
+      <p>Sháníme DrD+ pravidla pro Pohraničí. Můžete nám nějak
+        pomoct? <a href="mailto:info@drdplus.info">Napište nám <span class="far fa-envelope"></span></a>!</p>
+      <hr>
+      <p><a href="/">&lt;&lt; zpět</a></p>
+    </article>
+  </div>
+</div>
