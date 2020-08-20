9fb37571fe6fd266a5415d6e4e79b891bbfd6ec4
Jaroslav Týc <mail@jaroslavtyc.com>
2020-08-20 21:49:06 +0200

    Optimized titles images gray

--- js/items-with-background-image.js
+++ js/items-with-background-image.js
@@ -33,20 +33,31 @@ const bringOutBackgroundImageFromShadows = (element) => {
     element.style.backgroundImage = backgroundImageElement.style.backgroundImage
 }
 
-$(() => {
+window.addEventListener('DOMContentLoaded', () => {
     const elementsWithGrayBackgroundImage = document.getElementsByClassName('item-with-background-image')
     for (let length = elementsWithGrayBackgroundImage.length, index = 0; index < length; index++) {
         try {
             const item = elementsWithGrayBackgroundImage.item(index)
             moveBackgroundImageToLowerLayer(item)
             sentBackgroundImageToShadows(item)
-            $(item).hover(() => bringOutBackgroundImageFromShadows(item), () => sentBackgroundImageToShadows(item))
-            $(item).on('touchstart', () => bringOutBackgroundImageFromShadows(item))
-            $(item).on('touchend', () => sentBackgroundImageToShadows(item))
         } catch (error) {
             console.warn(error)
         }
     }
     const event = new Event('ItemsHaveBackgroundImagesInShadows')
     window.dispatchEvent(event)
+})
+
+jQuery(document).ready(() => {
+    const elementsWithGrayBackgroundImage = document.getElementsByClassName('item-with-background-image')
+    for (let length = elementsWithGrayBackgroundImage.length, index = 0; index < length; index++) {
+        try {
+            const item = elementsWithGrayBackgroundImage.item(index)
+            jQuery(item).hover(() => bringOutBackgroundImageFromShadows(item), () => sentBackgroundImageToShadows(item))
+            jQuery(item).on('touchstart', () => bringOutBackgroundImageFromShadows(item))
+            jQuery(item).on('touchend', () => sentBackgroundImageToShadows(item))
+        } catch (error) {
+            console.warn(error)
+        }
+    }
 })
